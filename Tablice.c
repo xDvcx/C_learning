a72c8c70c6f63e64420aed3fef0e9779d499431d
Filip Kowalewski <filip.kowalewski1@gmail.com>
2022-09-22 16:06:53 +0200

    Program wypisuje NR kolejnych liczb w odwrotnej kolejności

--- Tablice.c
+++ Tablice.c
@@ -1,1 +1,23 @@
-bla bla bla
+#include <stdio.h>
+
+#define NR 10
+
+//Program wypisuje NR kolejnych liczb w odwrotnej kolejności
+
+int main_test()
+{
+    int a[NR];
+    int i;
+    
+    for(i=0; i<NR; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    for(i=NR-1; i>=0; i--)
+    {
+        printf("%d ",a[i]);
+    }    
+    
+    return 0;
+}
